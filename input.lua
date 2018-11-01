bcaff3f49a36b27e273fa2bf36cf2fe358521bf8
chrismohrman <37715827+chrismohrman@users.noreply.github.com>
2018-10-31 23:38:33 -0400

    Finished
    finished the game

--- input.lua
+++ input.lua
@@ -1,59 +1,49 @@
 -- input.lua
 
--- This table is the service and will contain some functions
--- that can be accessed from entities or the main.lua.
-local input = {}
--- Map specific user inputs to game actions
-local press_functions = {}
-local release_functions = {}
+local state = require('state')
 
-
--- For moving paddle left
-input.left = false
--- For moving paddle right
-input.right = false
--- Keep track of whether game is pause
-input.paused = false
--- Look up in the map for actions that correspond to specific key presses
-input.press = function(pressed_key)
-  if press_functions[pressed_key] then
-    press_functions[pressed_key]()
+local press_functions = {
+  left = function()
+    state.button_left = true
+  end,
+  right = function()
+    state.button_right = true
   end
-end
--- Look up in the map for actions that correspond to specific key releases
-input.release = function(released_key)
-  if release_functions[released_key] then
-    release_functions[released_key]()
+  escape = function()
+    love.event.quit()
   end
-end
--- Handle window focusing/unfocusing
-input.toggle_focus = function(focused)
-  if not focused then
-    input.paused = true
+  space = function()
+    if state.game_over or state.stage_cleared then
+      return
+    end
+    state.paused = not state.paused
   end
-end
+}
 
+local release_functions = {
+  left = function()
+    state.button_left = false
+  end,
+  right = function()
+    state.button_right = false
+  end
+}
 
-press_functions.left = function()
-  input.left = true
-end
-press_functions.right = function()
-  input.right = true
-end
-press_functions.escape = function()
-  love.event.quit()
-end
-press_functions.space = function()
-  input.paused = not input.paused
-end
+return {
+  press = function(pressed_key)
+    if press_functions[pressed_key] then
+      press_functions[pressed_key]()
+    end
+  end,
+  release = function(released_key)
+    if release_functions[released_key] then
+      release_functions[released_key]()
+    end
+  end,
 
-
-release_functions.left = function()
-  input.left = false
-end
-release_functions.right = function()
-  input.right = false
-end
-
-
-return input
+  toggle_focus = function(focused)
+    if not focused then
+      state.paused = true
+    end
+  end
+}
--- main.lua
+++ main.lua
@@ -1,43 +1,30 @@
 -- main.lua
-
-local entities = require('entities')
-local input = require('input')
-local world = require('world')
-
-love.draw = function()
-  for _, entity in ipairs(entities) do
-    if entity.draw then entity:draw() end
-  end
-  if not input.paused then return
-  end
-  love.graphics.print("pause", 400, 300)
-end
-
-love.focus = function(focused)
-  input.toggle_focus(focused)
-end
-
-love.keypressed = function(pressed_key)
-  input.press(pressed_key)
-end
-
-love.keyreleased = function(released_key)
-  input.release(released_key)
-end
-
 love.update = function(dt)
-  if not input.paused then
-    local index = 1
-    while index <= #entities do
-      local entity = entities[index]
-      if entity.update then entity:update(dt) end
-      if entity.health == 0 then
-        table.remove(entities, index)
-        entity.fixture:destroy()
-      else
-        index = index + 1
-      end
-    end
-    world:update(dt)
+  if state.game_over or state.paused or state.stage_cleared then
+    return
   end
+
+  -- Switch to true if we have bricks left
+  local have_bricks = false
+
+  local index = 1
+  while index <= #entities do
+    local entity = entities[index]
+    if entity.type == 'brick' then have_bricks = true end
+    if entity.update then entity:update(dt) end
+    -- When an entity has no health (brick has been hit enough times
+    -- then we remove it from the list of entities. Don't increment
+    -- the index number if doing that though because we have shrunk
+    -- the table and made all the items shift down by 1 in the index.
+    if entity.health and entity.health < 1 then
+      table.remove(entities, index)
+      entity.fixture:destroy()
+    else
+      index = index + 1
+    end
+  end
+
+  -- Flag the stage cleared if there are no more bricks
+  state.stage_cleared = not have_bricks
+  world:update(dt)
 end
